0d51f337ef1e54726e34d4cd67d5fbd3cb96df06
Mohammad Ashif <mohammadashif510@gmail.com>
2023-01-03 09:23:39 +0600

    problem solved

--- insert-position.cpp
+++ insert-position.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
 using namespace std;
 
+int searchKey(int arr[], int n, int key)
+{
+    int s = 0;
+    int e = n - 1;
+    int mid;
+    while (s <= e)
+    {
+        mid = s + (e - s) / 2;
+        if (arr[mid] == key)
+        {
+            return mid;
+        }
+        else if (arr[mid] > key)
+        {
+            e = mid - 1;
+        }
+        else
+        {
+            s = mid + 1;
+        }
+    }
+    return -1;
+}
+
+// void print()
+
 int main()
 {
-    cout<<"I am coding with c++";
+    int arr[5] = {3, 4, 6, 7, 12};
+    int index = searchKey(arr, 5, 12);
+    cout << "index of key is : " << index << endl;
 }
